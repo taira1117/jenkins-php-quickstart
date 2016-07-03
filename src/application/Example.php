8cc07a28d2e4761a8bd67556fd0b0e0e3bd94c73
K.Taira <taira1117@gmail.com>
2016-07-03 12:29:49 +0900

    duplicate code test

--- src/application/Example.php
+++ src/application/Example.php
@@ -15,4 +15,32 @@ class Example {
 		return 'bar';
 	}
 
+    public function dupulicate_method1($file) {
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit;
+        }
+	}
+
+    public function dupulicate_method2($file) {
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit;
+        }
+	}
+
 }
