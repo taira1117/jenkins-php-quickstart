be2b6e3d22b8e10f097309a2bc7bd75153f76a5c
K.Taira <taira1117@gmail.com>
2016-07-03 11:33:44 +0900

    modify for check style error

--- src/application/Example.php
+++ src/application/Example.php
@@ -5,5 +5,14 @@ class Example {
 	public function foo() {
 		return 'bar';
 	}
-	
+
+    public function bar() {
+		return 'bar';
+	}
+
+    public function foobar() {
+        $hoge = "fjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjskljfjsdfjsldjflkdsjfjsklj";
+		return 'bar';
+	}
+
 }
